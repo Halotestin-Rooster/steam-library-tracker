25b3e83d3133bf762a04af96770a1a229630f40e
Halotestin-Rooster <roosterc43@gmail.com>
2026-04-10 21:30:30 +0430

    Add files via upload

---
+++ Web-Projects/api/games.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+    // Get access token
+    const tokenRes = await fetch(`https://id.twitch.tv/oauth2/token`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`
+    });
+
+    const tokenData = await tokenRes.json();
+    const accessToken = tokenData.access_token;
+
+    const { query } = req.body || {};
+
+    // Call IGDB
+    const igdbRes = await fetch('https://api.igdb.com/v4/games', {
+        method: 'POST',
+        headers: {
+            'Client-ID': CLIENT_ID,
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: `
+            search "${query || ''}";
+            fields name, rating, first_release_date, cover.url;
+            limit 24;
+        `
+    });
+
+    const data = await igdbRes.json();
+
+    res.status(200).json(data);
+}
